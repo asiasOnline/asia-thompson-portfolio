641a2f9691021bb33efd19328ab00becb6835a74
Asia Thompson <asiasatwork@gmail.com>
2024-06-10 07:10:48 -0500

    Add gradient background component and remove text highlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "../components/Hero"
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <BackgroundGradientAnimation />
     </main>
   );
 }
