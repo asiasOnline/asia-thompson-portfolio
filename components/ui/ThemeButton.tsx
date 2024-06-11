358f1419e0486e4124bcd4b1b06d1bdb1516996d
Asia Thompson <asiasatwork@gmail.com>
2024-06-10 21:47:16 -0500

    Add base functioning for dark mode

--- components/ui/ThemeButton.tsx
+++ components/ui/ThemeButton.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { PiSun, PiMoon } from "react-icons/pi";
+import { TbLoader2 } from "react-icons/tb";
 import { useState, useEffect } from 'react';
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/Button";
@@ -14,11 +15,22 @@ const ThemeButton = () => {
   useEffect(() => setMounted(true), [])
 
   if(!mounted) return (
-    <Button variant="outline">
-      <PiSun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'/>
-      <PiMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+    <Button disabled>
+      <TbLoader2 className='h-6 w-6 animate-spin'/>
     </Button>
   )
+
+  if (resolvedTheme === 'dark') {
+    return <Button variant="outline" size="icon" onClick={() => setTheme('light')}>
+              <PiSun className='h-6 w-6'/>
+          </Button>
+  }
+
+  if (resolvedTheme === 'light') {
+    return <Button variant="outline" size="icon" onClick={() => setTheme('dark')}>
+              <PiMoon className='h-6 w-6'/>
+          </Button>
+  }
 }
 
 export default ThemeButton
