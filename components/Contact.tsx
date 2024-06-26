9bc56c2eb748895b22ff32c559650e39991b83b4
Asia Thompson <asiasatwork@gmail.com>
2024-06-25 23:03:35 -0500

    Add Input, Textarea, Label, and Form ui components for Contact component form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import PageHeader from './ui/PageHeader'
+import { TbMail, TbMailFilled } from "react-icons/tb";
 
 const Contact = () => {
   return (
     <div id="contact">
         <PageHeader title='Contact'/>
+        <div>
+          <div>
+            <h2>Let's Work Together!</h2>
+            <p>Fill out the form to get in contact me as soon as possible. Otherwise, contact me through your preferred platform and I’ll do my best to follow up there as well.</p>
+            <div>
+              <TbMailFilled className='w-8 h-8'/>
+            </div>
+          </div>
+          <div>
+
+          </div>
+        </div>
     </div>
   )
 }
