30fafbc32728054a3ae1eba8fd8b2bdf1e5ef0cc
asiasAtWork <asiasatwork@gmail.com>
2024-07-10 08:03:05 -0500

    Format project overview component

--- app/projects/bee-tea/page.tsx
+++ app/projects/bee-tea/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { MdEmojiFoodBeverage } from "react-icons/md";
 import ProjectHero from '@/components/ProjectHero'
 import { BeeTeaOverviewVideo } from '@/components/ui/VideoAssets';
 import { BeeTeaLogo } from '@/components/ui/ProjectLogoAssets';
+import ProjectOverview from '@/components/ProjectOverview';
 
 
 const BeeTeaPage = () => {
@@ -15,6 +17,15 @@ const BeeTeaPage = () => {
       >
         <BeeTeaOverviewVideo />
       </ProjectHero>
+      <ProjectOverview 
+        clientBackground='Bee & Tea began as a passion project fueled by a strong commitment to sustainability, quality, community, along with the founder’s personal love of beekeeping and gardening. 
+
+Starting from humble beginnings as their popularity has grown they’ve stayed true to their values and methods ensuring their products are free from any additives and preservatives. 
+
+Their goal is to inspire healthy living and environmental stewardship through their products sourced from their own maintained hives and seasonal tea blends.'
+        industryIcon={<MdEmojiFoodBeverage />}
+        platformWebsite={true}
+      />
     </>
   )
 }
--- components/ProjectOverview.tsx
+++ components/ProjectOverview.tsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import { TbWorld } from "react-icons/tb";
 
-const ProjectOverview = () => {
+const website = {
+  type: "Website",
+  icon: <TbWorld />
+}
+
+const ProjectOverview = ({
+  clientBackground,
+  industryIcon,
+  industryTitle,
+  platformWebsite,
+  problemIntro,
+  problemExamples,
+  solutionExamples,
+  solutionIntro,
+} : {
+  clientBackground?: string;
+  industryIcon?: React.ReactNode; 
+  industryTitle?: string;
+  platformWebsite: boolean;
+  problemIntro?: string;
+  problemExamples?: React.ReactNode;
+  solutionExamples?: React.ReactNode;
+  solutionIntro?: string;
+}) => {
   return (
     <div>
-        
+        <h1>Overview</h1>
+        <h2>Client Background</h2>
+        <p>{clientBackground}</p>
+        <div>
+          <div>
+            <h2>Industry</h2>
+            <div className='flex'>{industryIcon}{industryTitle}</div>
+          </div>
+          <div>
+            <h2>Platforms</h2>
+            {platformWebsite ? website.icon : ''}
+          </div>
+        </div>
+        <div>
+          <h2>The Problem</h2>
+          {problemIntro}
+        </div>
+        {problemExamples}
+        <div>
+          <h2>The Solution</h2>
+          {solutionIntro}
+        </div>
+        {solutionExamples}
+        <div>
+          <div>
+            <h2>Timeline</h2>
+          </div>
+          <div>
+            <h2>Roles</h2>
+          </div>
+          <div>
+            <h2>Toolkit</h2>
+          </div>
+        </div>
     </div>
   )
 }
