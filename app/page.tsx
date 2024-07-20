debb514954f8a8272d37a039c0fe8d74956b8ab1
Asia Thompson <asiasatwork@gmail.com>
2024-07-20 12:45:05 -0500

    Add packages and initial content for Contact component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero"
 import Navbar from "@/components/Navbar";
 import Expertise from "@/components/Expertise";
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import PageHeader from './ui/PageHeader'
+import ContactLinks from './ui/ContactLinks'
+import HeroAvatar from './ui/HeroAvatar'
+import { ContactForm } from './ui/ContactForm'
 
 const Contact = () => {
   return (
     <div>
         <PageHeader title='Contact'/>
+        <div className='flex'>
+          <div className='w-1/2 flex flex-col'>
+            <h1>Let's Work Together!</h1>
+            <p>Fill out the form to get in contact me as soon as possible.</p>
+            <p>Otherwise, contact me through one of my socials and I’ll do my best to follow up there as well. </p>
+            <ContactLinks />
+            <div className='flex'>
+              <HeroAvatar />
+              <div>
+                <h2>My Timezone</h2>
+                <p>4:03 in Anaheim, CA</p>
+                <p>If you contact me now I'll typically respond immediately!</p>
+              </div>
+            </div>
+          </div>
+          <div className='w-1/2'>
+            <ContactForm />
+          </div>
+        </div>
     </div>
   )
 }
