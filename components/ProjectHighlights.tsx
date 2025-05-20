bd19b238f64636e6b2549887a0045fc2e2a244dd
asiasAtWork <asiasatwork@gmail.com>
2025-05-19 17:34:22 -0700

    Update project section to new layout

--- components/ProjectHighlights.tsx
+++ components/ProjectHighlights.tsx
@@ -6,10 +6,10 @@ const ProjectHighlights = () => {
   return (
     <div id="projectHighlights" className='w-full flex flex-col items-center'>
         <div id="projectHighlightsHeader" 
-            className='w-1/2 flex align-content-center justify-content-center'>
+            className='w-2/5 flex align-content-center justify-content-center'>
           <div>
             <h3 className='text-6xl'>Highlighted Work</h3>
-            <p className='w-4/5'>I only accept projects from businesses and within industries that I find interesting, so my projects are a great way to gain a little more insight into who I am.</p>
+            <p className='mt-4 mr-4'>I only accept projects from businesses and within industries that I find interesting, so my projects are a great way to gain a little more insight into who I am.</p>
           </div>
           <div className="relative w-full max-w-40 h-auto">
             < HeroAvatar />
--- components/ProjectScroll.tsx
+++ components/ProjectScroll.tsx
@@ -1,55 +1,96 @@
 import React from 'react'
 import { Button } from './ui/Button'
-import LocomotiveScroll from './ui/LocomotiveScroll'
+import Image from 'next/image'
 
-const projectImages = [
-      { src: "/project-assets/holistic-hands-project-cover.png", 
-      alt: "reiki healing website project cover",
-      square: false},
-
-      { src: "/project-assets/warm-wood-project-cover.png", 
-      alt: "reiki healing website project cover",
-      square: false},
-
-      { src: "/project-assets/mm-florist-project-cover.png", 
-      alt: "reiki healing website project cover",
-      square: false},
+const projects = [
+      { 
+      id: "1",
+      title: "Holistic Hands",  
+      year: "2024",
+      industry: "Health",
+      jobTags: ["Branding", "Web Design"],
+      description: "Lorem ipsum dolor sit amet consectetur. Lacus velit mi vitae mi amet est orci leo eget. Arcu faucibus nam pulvinar venenatis amet in scelerisque.",
+      image: {
+        src: "/project-assets/holistic-hands-project-cover.png", 
+        alt: "reiki healing website project cover",
+        square: false
+      }
+    },
+    { 
+      id: "2",
+      title: "Bee & Tea",  
+      year: "2022",
+      industry: "Ecommerce",
+      jobTags: ["Branding", "Web Design", "Web Development"],
+      description: "Lorem ipsum dolor sit amet consectetur. Lacus velit mi vitae mi amet est orci leo eget. Arcu faucibus nam pulvinar venenatis amet in scelerisque.",
+      image: {
+        src: "/project-assets/bee-tea-project-cover.png", 
+        alt: "bee tea project cover",
+        square: false
+      }
+    },
+      { 
+      id: "3",
+      title: "MM Florist",  
+      year: "2021",
+      industry: "Ecommerce",
+      jobTags: ["Web Design", "Web Development"],
+      description: "Lorem ipsum dolor sit amet consectetur. Lacus velit mi vitae mi amet est orci leo eget. Arcu faucibus nam pulvinar venenatis amet in scelerisque.",
+      image: {
+        src: "/project-assets/mm-florist-project-cover.png", 
+        alt: "MM florist project cover",
+        square: false
+      }
+    },
     ]
 
 const ProjectScroll = () => {
   return (
-    <div className='w-full flex justify-between px-6 max-h-[520px] border-black border-y-2'>
-        <div className='w-2/5 h-full my-6 flex flex-col justify-around border-black'>
+    <div className='w-full gap-16 gap-lg-0 flex flex-col justify-between px-6 max-h-[600px] border-black border-y-2 overflow-y-auto'>
+      {projects.map((project, index) => {
+        return <div className='flex my-8'>
+          {/*Project Details*/}
+          <div className='w-1/2'>
             <div>
-                <p className='italic'>02/05</p>
-            </div>
-            <div className='flex flex-col gap-2 border-black border-y-2 py-16'>
-              <div className='w-full flex justify-between pb-4'>
-              <p>2024</p>
-              <p className='font-bold'>HEALTH</p>
+            <p className='italic'>{project.id}/{projects.length}</p>
+          </div>
+          <div className='flex flex-col gap-6 border-slate-500 border-y-2 py-12'>
+            <div className='w-full flex justify-between pb-4'>
+              <p>{project.year}</p>
+              <p className='font-bold'>{project.industry}</p>
             </div>
             <div>
-              <h4 className='text-7xl'>Holistic Hands</h4>
+              <h4 className='text-7xl'>{project.title}</h4>
             </div>
             <div className='flex gap-4'>
-              <p>Branding</p>
-              <p>Web Design</p>
+              {project.jobTags.map((tag) => {
+                  return <div className='p-2 bg-slate-400 rounded-lg'>{tag}</div>
+                })}
             </div>
             <div>
-              <p>Lorem ipsum dolor sit amet consectetur. Lacus velit mi vitae mi amet est orci leo eget. Arcu faucibus nam pulvinar venenatis amet in scelerisque.</p>
+              <p className='text-xl'>{project.description}</p>
             </div>
             <Button 
-          variant="outline" 
-          type="submit" 
-          className="text-md w-1/2 gap-4 font-bold tracking-wide p-6 border-2 border-black button-shadow-xl dark:bg-black dark:text-white dark:border-white dark:button-shadow-none"
-          >View My Projects
-        </Button>
-            
-            </div>
+              variant="outline" 
+              type="submit" 
+              className="text-md w-2/5 gap-4 font-bold tracking-wide p-6 border-2 border-black button-shadow-xl dark:bg-black dark:text-white dark:border-white dark:button-shadow-none"
+              >View My Projects
+          </Button>
+          </div>
+          </div>
+          {/*Project Image*/}
+          <div className='w-1/2 flex justify-center'>
+            <Image 
+              key={index}
+              src={project.image.src}
+              alt={project.image.alt}
+              width={720}
+              height={405}
+              className="border-2 border-black rounded-md"
+              />
+          </div>
         </div>
-        <div className="w-1/2 relative pt-10 pb-14 overflow-scroll">
-                <LocomotiveScroll images={projectImages} />
-            </div>
+      })}
     </div>
   )
 }
