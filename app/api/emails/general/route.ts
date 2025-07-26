96475c0fcf1d6d005bd7b9ef1c25cf578e81545d
Asia Thompson <asiasatwork@gmail.com>
2025-07-25 22:15:58 -0700

    Fix case-sensitive import for Received email import

--- app/api/emails/general/route.ts
+++ app/api/emails/general/route.ts
@@ -1,4 +1,4 @@
-import  Received from '@/emails/Received'
+import  Received from '../../../../emails/Received'
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY)
