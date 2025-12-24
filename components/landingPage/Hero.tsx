a2911424664be7ebcbfcf62934ccd45f91298f10
Asia Thompson <contact@asiasonline.com>
2025-12-23 23:48:02 -0800

    install packages and modify hero text

--- components/landingPage/Hero.tsx
+++ components/landingPage/Hero.tsx
@@ -75,49 +75,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <div id="hero" className='w-full 2xl:w-4/5 relative pt-6 2xl:pt-20 px-4 md:px-16 lg:px-24 xl:px-32 mx-auto flex flex-col gap-6 '>
-      {/*Borders*/}
-      {/*Top Left
-      <div className='hidden xl:block absolute xl:top-8 xl:-left-0'>
-        <div className='w-44 relative left-14'>
-            <hr className='border-t-black dark:border-t-white'/>
-        </div>
-        < div className='w-44 rotate-90 relative top-14'>
-            <hr className='border-t-black dark:border-t-white'/>
-          </div>
-        </div>
-        */}
-        {/*Top Right
-        <div className='hidden xl:block absolute rotate-90 top-8 right-0'>
-          <div className='w-44 relative left-14'>
-            <hr className='border-t-black dark:border-t-white'/>
-          </div>
-          <div className='w-44 rotate-90 relative top-14'>
-              <hr className='border-t-black dark:border-t-white'/>
-          </div>
-        </div>
-        */}
-        {/*Bottom Right
-        <div className='hidden xl:block absolute rotate-180 right-0 -bottom-8'>
-          <div className='w-44 relative left-14'>
-            <hr className='border-t-black dark:border-t-white'/>
-          </div>
-          <div className='w-44 rotate-90 relative top-14'>
-              <hr className='border-t-black dark:border-t-white'/>
-          </div>
-        </div>
-        */}
-        {/*Bottom Left
-        <div className='hidden xl:block absolute -rotate-90 left-0 -bottom-8'>
-          <div className='w-44 relative left-14'>
-            <hr className='border-t-black dark:border-t-white'/>
-        </div>
-        <div className='w-44 rotate-90 relative top-14'>
-            <hr className='border-t-black dark:border-t-white'/>
-        </div>
-        </div>
-        */}
-        
+    <div id="hero" className='w-full 2xl:w-4/5 relative pt-6 2xl:pt-20 px-4 md:px-16 lg:px-24 xl:px-32 mx-auto flex flex-col gap-6 '>  
         {/*Main Hero Content*/}
         <div className='xl:mx-10 2xl:m-0 flex flex-col items-center gap-6 2xl:gap-12'>
           
@@ -127,7 +85,7 @@ const Hero = () => {
                 <p
                 className='text-center text-lg md:text-xl 2xl:text-2xl'
               >
-                My name is <strong>Asia</strong>.
+                Hello, my name is...
               </p>
               </div>
               <div className='w-auto md:w-40 h-auto flex justify-center'>
@@ -155,8 +113,7 @@ const Hero = () => {
               <h1 
               ref={headlineRef}
               className='text-3xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-nowrap font-display font-bold tracking-wider'>
-              I Help Creatives Build
-              
+              Asia Thompson
               </h1>
 
               {/*
